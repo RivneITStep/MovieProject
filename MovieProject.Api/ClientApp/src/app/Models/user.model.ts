8c8e061358042e252c77260a3173b8a9efdec95a
Andrey Ryzhuk <senseandrey@gmail.com>
2021-02-07 03:27:34 +0200

    add: user profile

--- MovieProject.Api/ClientApp/src/app/Models/user.model.ts
+++ MovieProject.Api/ClientApp/src/app/Models/user.model.ts
@@ -1,9 +1,13 @@
 export class UserModel{
     public id: string;
     public email: string;
+    public pictureUrl: string;
+    public balance: number;
 
     constructor(){
         this.id = null;
         this.email = null;
+        this.pictureUrl = null;
+        this.balance = null;
     }
 }
